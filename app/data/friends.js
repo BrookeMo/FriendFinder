93fbe3c6357790f47e3e8e6086a95ae00d818a7e
Brooke Mortensen <brookeamymo@gmail.com>
2018-04-25 10:40:24 -0600

    added friends.js friendsArray

---
+++ app/data/friends.js
@@ -0,0 +1,20 @@
+var friendsArray = [
+	{
+		"name":"Brooke",
+		"photo":"https://www.facebook.com/photo.php?fbid=10215556963897125&l=53564d909d",
+		"scores":[
+				5,
+				2,
+				1,
+				2,
+				2,
+				1,
+				5,
+				5,
+				3,
+				4
+			]
+	}	
+];
+
+module.exports = friendsArray;
